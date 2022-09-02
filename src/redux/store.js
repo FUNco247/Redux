c8c437c554afda1dc9586e044d23a9ea6880fad6
FUNco <crispy.alc@gmail.com>
2022-09-02 22:56:49 +0900

    ENH using createReducer func in redux-toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { legacy_createStore as createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 /* export const addToDo = (text, id) => {
   return {
@@ -18,8 +18,8 @@ export const deleteToDo = (id) => {
 
 export const addToDo = createAction("ADD");
 export const deleteToDo = createAction("DELETE");
-console.log(addToDo(), deleteToDo());
-const reducer = (state = [], action) => {
+//console.log(addToDo(), deleteToDo());
+/* const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case "ADD":
@@ -29,7 +29,17 @@ const reducer = (state = [], action) => {
     default:
       return state;
   }
-};
+}; */
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.unshift({ text: action.payload.text, id: action.payload.id });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
+// when you use createReducer func, you can mutate state .... "Immer" inside redux toolkit helps you, it will return new state
+// *So* when you mutate your state.. don't return it by your self. Let Immer do that
+
 const store = createStore(reducer);
 
 export default store;
